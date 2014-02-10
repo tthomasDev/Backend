2b96b876034729bb859289f16fe22d219ab13fbd
adelesse <alex.delesse@gmail.com>
2014-02-10 16:50:08 +0100

    Ajout de classes dans le modele

---
+++ my-neightool/src/main/java/com/ped/myneightool/model/Outil.java
@@ -0,0 +1,81 @@
+package com.ped.myneightool.model;
+
+public class Outil {
+	
+	private int id;
+	private int idProprio;
+	private String nom;
+	private String description;
+	private boolean disponible;
+	private String categorie;
+	private int caution;
+
+	public Outil(int id, int idProprio, String nom, String description,
+			boolean disponible, String categorie, int caution) {
+		super();
+		this.id = id;
+		this.idProprio = idProprio;
+		this.nom = nom;
+		this.description = description;
+		this.disponible = disponible;
+		this.categorie = categorie;
+		this.caution = caution;
+	}
+
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getNom() {
+		return nom;
+	}
+	
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	
+	public boolean isDisponible() {
+		return disponible;
+	}
+
+	public void setDisponible(boolean disponible) {
+		this.disponible = disponible;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+	
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getCategorie() {
+		return categorie;
+	}
+
+	public void setCategorie(String categorie) {
+		this.categorie = categorie;
+	}
+
+	public int getCaution() {
+		return caution;
+	}
+
+	public void setCaution(int caution) {
+		this.caution = caution;
+	}
+
+	public int getIdProprio() {
+		return idProprio;
+	}
+
+	public void setIdProprio(int idProprio) {
+		this.idProprio = idProprio;
+	}
+
+}
---
+++ my-neightool/src/main/java/com/ped/myneightool/model/Utilisateur.java
@@ -0,0 +1,73 @@
+package com.ped.myneightool.model;
+
+public class Utilisateur {
+	
+	private int id;
+	private String nom;
+	private String prenom;
+	private Connection connexion;
+	private String mail;
+	private String telephone;
+	
+	public Utilisateur(int id, String nom, String prenom, Connection connexion,
+			String mail, String telephone) {
+		super();
+		this.id = id;
+		this.nom = nom;
+		this.prenom = prenom;
+		this.connexion = connexion;
+		this.mail = mail;
+		this.telephone = telephone;
+	}
+
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getNom() {
+		return nom;
+	}
+	
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	
+	public String getPrenom() {
+		return prenom;
+	}
+	
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	public Connection getConnexion() {
+		return connexion;
+	}
+
+	public void setConnexion(Connection connexion) {
+		this.connexion = connexion;
+	}
+
+	public String getMail() {
+		return mail;
+	}
+
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+
+	public String getTelephone() {
+		return telephone;
+	}
+
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+
+	
+
+}
