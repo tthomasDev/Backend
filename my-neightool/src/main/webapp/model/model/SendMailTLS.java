ddf86e87d4d15f8e38c65cdb6be976abdf4e4e8c
adelesse <adelesse@sulu.emi.u-bordeaux1.fr>
2014-02-21 12:39:51 +0100

    pour moi bosser plus tard chez moi préparation mail confirmation (non fonctionnel encore)

--- my-neightool/src/main/webapp/model/model/SendMailTLS.java
+++ my-neightool/src/main/webapp/model/model/SendMailTLS.java
@@ -1,4 +1,4 @@
-package com.ped.myneightool;
+package model;
 
 import java.util.Properties;
 
@@ -11,34 +11,37 @@ import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
  
 public class SendMailTLS {
+	
+	public SendMailTLS (String dest,String myMessage)
+	{
+		System.out.println("Envoi d'un message");
  
-	public static void main(String[] args) {
- 
-		final String username = "myneightool@gmail.com";
-		final String password = "A METTRE MDP";/* TODO */
+		final String username = "alex.delesse@gmail.com";
+		final String password = "TODO";/* TODO */
  
 		Properties props = new Properties();
 		props.put("mail.smtp.auth", "true");
 		props.put("mail.smtp.starttls.enable", "true");
 		props.put("mail.smtp.host", "smtp.gmail.com");
 		props.put("mail.smtp.port", "587");
- 
+		
+		System.out.println("propriétés fait");
+		
 		Session session = Session.getInstance(props,
 		  new javax.mail.Authenticator() {
 			protected PasswordAuthentication getPasswordAuthentication() {
 				return new PasswordAuthentication(username, password);
 			}
 		  });
- 
+		System.out.println("Autentification fait");
 		try {
  
 			Message message = new MimeMessage(session);
-			message.setFrom(new InternetAddress("alex.delesse@gmail.com"));
+			message.setFrom(new InternetAddress("myneightool@gmail.com"));
 			message.setRecipients(Message.RecipientType.TO,
-				InternetAddress.parse("alex.delesse@gmail.com"));
+				InternetAddress.parse(dest));
 			message.setSubject("Testing Subject");
-			message.setText("Dear Mail Crawler,"
-				+ "\n\n No spam to my email, please!");
+			message.setText(myMessage);
  
 			Transport.send(message);
  
