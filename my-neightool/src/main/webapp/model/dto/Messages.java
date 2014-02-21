8880fb8214150de42664a9b337fd03837ed67812
tthomas <tthomas@spock.emi.u-bordeaux1.fr>
2014-02-21 11:00:06 +0100

    ajout nouvelle dto et webservices pour les messages triés par ordre DESC

--- my-neightool/src/main/webapp/model/dto/Messages.java
+++ my-neightool/src/main/webapp/model/dto/Messages.java
@@ -1,13 +1,16 @@
-package com.ped.myneightool.dto;
+package dto;
 
 
 
 import java.util.ArrayList;
 import java.util.List;
+
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
-import com.ped.myneightool.model.Message;
+import model.Message;
+
+
 
 
 
