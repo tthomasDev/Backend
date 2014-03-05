a2ae6dbf8ce7b632acf71c3a23438f5656c5eb4d
adelesse <adelesse@pippin.emi.u-bordeaux1.fr>
2014-03-05 13:30:04 +0100

    constructeur avec objet pour sendmailTLS

--- my-neightool/src/main/webapp/model/model/SendMailTLS.java
+++ my-neightool/src/main/webapp/model/model/SendMailTLS.java
@@ -14,10 +14,10 @@ public class SendMailTLS {
 	
 	public SendMailTLS (String dest,String myMessage)
 	{
-		System.out.println("Envoi d'un message");
+		System.out.println("Envoi d'un message1");
  
-		final String username = "myneightool@gmail.com";
-		final String password = "";/*TODO*/
+		final String username = "myneightool@gmail.com";/*TODO*/
+		final String password = "ped2014tool";
  
 		Properties props = new Properties();
 		props.put("mail.smtp.auth", "true");
@@ -47,6 +47,49 @@ public class SendMailTLS {
 			Transport.send(message);
 			
 			System.out.println("Message envoyé");
+			System.out.println("Done");
+ 
+		} catch (MessagingException e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	
+	public SendMailTLS (String dest,String myMessage, String myObjet)
+	{
+		System.out.println("Envoi d'un message2");
+		String objet=myObjet;
+		final String username = "myneightool@gmail.com";
+		final String password = "ped2014tool";/*TODO*/
+ 
+		Properties props = new Properties();
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.starttls.enable", "true");
+		props.put("mail.smtp.host", "smtp.gmail.com");
+		props.put("mail.smtp.port", "587");
+		
+		System.out.println("PROPRIETE OK2");
+		
+		Session session = Session.getInstance(props,
+		  new javax.mail.Authenticator() {
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(username, password);
+			}
+		  });
+		System.out.println("Autentification fait2");
+		try {
+ 
+			Message message = new MimeMessage(session);
+			message.setFrom(new InternetAddress("myneightool@gmail.com"));
+			message.setRecipients(Message.RecipientType.TO,
+				InternetAddress.parse(dest));
+			message.setSubject(objet);
+			message.setText(myMessage);
+			
+			System.out.println("Message créé2");
+			Transport.send(message);
+			
+			System.out.println("Message envoyé2");
 			System.out.println("Done");
  
 		} catch (MessagingException e) {
